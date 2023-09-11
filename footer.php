443fad89ba61e3dd88c43dc0261864764073bfcb
bora <nbora617@naver.com>
2023-09-11 14:57:28 +0900

    last

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="left">
+        <h1><a href="#"><img src="./images/f_logo.png" alt="드롭탑"></a></h1>
+        <div class="f_sns">
+            <a href="#"><img src="./images/sns1.png" alt="인스타그램"></a>
+            <a href="#"><img src="./images/sns2.png" alt="네이버블로그"></a>
+            <a href="#"><img src="./images/sns3.png" alt="유튜브"></a>
+        </div>
+    </div>
+    <div class="right">
+        <div class="f_nav">
+            <a href="company_intro.html">회사소개</a>
+            <a href="#">개인정보취급방침</a>
+            <a href="#">이용약관</a>
+            <a href="#">본/지사안내</a>
+            <a href="#">채용 공고</a>
+            <a href="#">시공업체등록</a>
+            <a href="#">사이트맵</a>
+        </div>
+        <address>서초구 강남대로 601 (잠원동 21-2) 8층, (주)다도글로벌/드롭탑 | T.(02)541-7800
+            사업자등록번호 : 211-88-47165 | 대표이사 : 임문수 | 개인정보관리책임자 : 김보철 | 통신판매업번호 : 2019-서울서초-1518
+            마케팅∙제휴 문의 top_mkt@dadoglobal.com</address>
+        <p>Copyright ⓒ 2021 DROPTOP All Rights Reserved.</p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,110 @@
+<header>
+    <h1><a href="index.html"><img src="./images/main_logo.png" alt="드롭탑"></a></h1>
+    <nav>
+        <ul class="main_m">
+            <li>
+                <a href="#">드롭탑</a>
+                <ul class="sub_m">
+                    <li><a href="company_intro.html">회사소개</a></li>
+                    <li><a href="#">925 커피이야기</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="menu.html">메뉴</a>
+                <ul class="sub_m">
+                    <li><a href="menu.html">신제품</a></li>
+                    <li><a href="menu.html">커피</a></li>
+                    <li><a href="menu.html">음료</a></li>
+                    <li><a href="menu.html">푸드</a></li>
+                    <li><a href="menu.html">케이크</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="store.html">매장</a>
+            </li>
+            <li>
+                <a href="#">이벤트</a>
+                <ul class="sub_m">
+                    <li><a href="#">이벤트 소식</a></li>
+                    <li><a href="#">당첨자 발표</a></li>
+                    <li><a href="#">제휴카드</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">커뮤니티</a>
+                <ul class="sub_m">
+                    <li><a href="#">공지사항</a></li>
+                    <li><a href="#">보도자료</a></li>
+                    <li><a href="#">제작지원</a></li>
+                    <li><a href="#">멀티미디어</a></li>
+                    <li><a href="#">소셜 드롭탑</a></li>
+                    <li><a href="#">고객/점주 소리</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <a href="#" class="link">가맹안내</a>
+    <!-- 모바일메뉴 아이콘 -->
+    <a href="#" class="m_nav"><img src="./images/m_nav.png" alt="모바일메뉴"></a>
+    <!-- 모바일 메뉴창 -->
+    <div class="open_nav">
+        <button type="reset" id="close"><img src="./images/close.png" alt="닫기"></button>
+        <ul class="m_menu">
+            <li>
+                <a href="#">드롭탑</a>
+                <ul class="sub_m">
+                    <li><a href="title brand_tab active">회사소개</a></li>
+                    <li><a href="#">925 커피이야기</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">메뉴</a>
+                <ul class="sub_m">
+                    <li><a href="menu.html">신제품</a></li>
+                    <li><a href="menu.html">커피</a></li>
+                    <li><a href="menu.html">음료</a></li>
+                    <li><a href="menu.html">푸드</a></li>
+                    <li><a href="menu.html">케이크</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="store.html">매장</a>
+            </li>
+            <li>
+                <a href="#">이벤트</a>
+                <ul class="sub_m">
+                    <li><a href="#">이벤트 소식</a></li>
+                    <li><a href="#">당첨자 발표</a></li>
+                    <li><a href="#">제휴카드</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">커뮤니티</a>
+                <ul class="sub_m">
+                    <li><a href="#">공지사항</a></li>
+                    <li><a href="#">보도자료</a></li>
+                    <li><a href="#">제작지원</a></li>
+                    <li><a href="#">멀티미디어</a></li>
+                    <li><a href="#">소셜 드롭탑</a></li>
+                    <li><a href="#">고객/점주의 소리</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="btm">
+            <div class="pop_icon">
+                <div class="talk"><img src="./images/talk.png" alt=""><span>고객문의</span></div>
+                <div class="location"><img src="./images/location.png" alt=""><span>매장찾기</span></div>
+                <div class="mall"><img src="./images/mall.png" alt=""><span>온라인몰</span></div>
+                <div class="call"><img src="./images/call.png" alt=""><span>가맹문의</span></div>
+            </div>
+            <div class="link_btn">
+                <a href="#">가맹안내</a>
+                <div class="sns">
+                    <a href="#"><img src="./images/sns1.png" alt="인스타그램"></a>
+                    <a href="#"><img src="./images/sns2.png" alt="블로그"></a>
+                    <a href="#"><img src="./images/sns3.png" alt="유튜브"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
