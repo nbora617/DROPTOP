9be8a100029125c13efc7288009834f98135da89
bora <nbora617@naver.com>
2023-09-11 17:19:23 +0900

    end

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <nav>
         <ul class="main_m">
             <li>
-                <a href="#">드롭탑</a>
+                <a href="company_intro.html">드롭탑</a>
                 <ul class="sub_m">
                     <li><a href="company_intro.html">회사소개</a></li>
                     <li><a href="#">925 커피이야기</a></li>
@@ -51,14 +51,14 @@
         <button type="reset" id="close"><img src="./images/close.png" alt="닫기"></button>
         <ul class="m_menu">
             <li>
-                <a href="#">드롭탑</a>
+                <a href="company_intro.html">드롭탑</a>
                 <ul class="sub_m">
-                    <li><a href="title brand_tab active">회사소개</a></li>
+                    <li><a href="company_intro.html">회사소개</a></li>
                     <li><a href="#">925 커피이야기</a></li>
                 </ul>
             </li>
             <li>
-                <a href="#">메뉴</a>
+                <a href="menu.html">메뉴</a>
                 <ul class="sub_m">
                     <li><a href="menu.html">신제품</a></li>
                     <li><a href="menu.html">커피</a></li>
@@ -92,17 +92,17 @@
         </ul>
         <div class="btm">
             <div class="pop_icon">
-                <div class="talk"><img src="./images/talk.png" alt=""><span>고객문의</span></div>
-                <div class="location"><img src="./images/location.png" alt=""><span>매장찾기</span></div>
-                <div class="mall"><img src="./images/mall.png" alt=""><span>온라인몰</span></div>
-                <div class="call"><img src="./images/call.png" alt=""><span>가맹문의</span></div>
+                <a href="#" class="talk"><img src="./images/talk.png" alt=""><span>고객문의</span></a>
+                <a href="store.html" class="location"><img src="./images/location.png" alt=""><span>매장찾기</span></a>
+                <a href="#" class="mall"><img src="./images/mall.png" alt=""><span>온라인몰</span></a>
+                <a href="#" class="call"><img src="./images/call.png" alt=""><span>가맹문의</span></a>
             </div>
             <div class="link_btn">
                 <a href="#">가맹안내</a>
                 <div class="sns">
-                    <a href="#"><img src="./images/sns1.png" alt="인스타그램"></a>
-                    <a href="#"><img src="./images/sns2.png" alt="블로그"></a>
-                    <a href="#"><img src="./images/sns3.png" alt="유튜브"></a>
+                    <a href="#"><img src="./images/m_sns1.png" alt="인스타그램"></a>
+                    <a href="#"><img src="./images/m_sns2.png" alt="블로그"></a>
+                    <a href="#"><img src="./images/m_sns3.png" alt="유튜브"></a>
                 </div>
             </div>
         </div>
